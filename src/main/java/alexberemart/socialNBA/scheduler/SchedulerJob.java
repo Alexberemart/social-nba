4f08319147075f29bb5b3cc5b3698d33b1012f80
Alejandro Berenguer <alexberemart@gmail.com>
2015-10-22 08:24:09 +0200

    add quartz configuration

---
+++ src/main/java/alexberemart/socialNBA/scheduler/SchedulerJob.java
@@ -0,0 +1,25 @@
+package alexberemart.socialNBA.scheduler;
+
+import alexberemart.socialNBA.services.TwitServices;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import twitter4j.TwitterException;
+
+//TODO: Esposible mover el fichero de configuración del quartz al paquete que le corresponte?, si lo pones ahi no rula.  http://www.mkyong.com/jsf2/jsf-2-quartz-2-example/
+
+public class SchedulerJob implements Job {
+
+	@Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+		
+		System.out.println("JSF 2 + Quartz 2 example");
+        try {
+            TwitServices.getInstance().getPlayerTwits();
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
