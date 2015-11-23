17577cc94e35966566fa9fd1bf173b2cacaba4f9
Alejandro Berenguer <alexberemart@gmail.com>
2015-11-23 16:00:53 +0100

    add date attribute to basketballReference match

--- src/main/java/alexberemart/socialNBA/model/vo/basketReference/Match.java
+++ src/main/java/alexberemart/socialNBA/model/vo/basketReference/Match.java
@@ -1,11 +1,13 @@
 package alexberemart.socialNBA.model.vo.basketReference;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Match {
 
     protected String key;
+    protected Date date;
     List<TeamStats> teamEntries = new ArrayList<>();
 
     public String getKey() {
@@ -22,5 +24,13 @@ public class Match {
 
     public void setTeamEntries(List<TeamStats> teamEntries) {
         this.teamEntries = teamEntries;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
