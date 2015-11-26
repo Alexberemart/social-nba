f3411ef1e03207cf6b67884fcb5e8e4278ecd3c0
Alejandro Berenguer <alexberemart@gmail.com>
2015-11-26 16:06:04 +0100

    Update RestServiceLog POJO to create services for the front

--- src/main/java/alexberemart/socialNBA/model/dao/impl/RestServiceLogDAOImpl.java
+++ src/main/java/alexberemart/socialNBA/model/dao/impl/RestServiceLogDAOImpl.java
@@ -1,13 +1,64 @@
 package alexberemart.socialNBA.model.dao.impl;
 
 import Alexberemart.core.model.dao.base.hibernate.spring.impl.GenericHibernateSpringDAOImpl;
+import Alexberemart.core.util.StringUtils;
 import alexberemart.socialNBA.model.dao.RestServiceLogDAO;
 import alexberemart.socialNBA.model.vo.RestServiceLog;
+import alexberemart.socialNBA.util.IntegerUtils;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+
+import java.util.List;
 
 public class RestServiceLogDAOImpl extends GenericHibernateSpringDAOImpl<RestServiceLog, String> implements RestServiceLogDAO {
 
     public RestServiceLogDAOImpl() {
         super(RestServiceLog.class);
+    }
+
+    @Override
+    public List<RestServiceLog> findWithFiltersPaginated(String orderBy, Integer offset, Integer perPage, Boolean asc, String search) {
+
+        DetachedCriteria detachedCriteria = DetachedCriteria
+                .forClass(RestServiceLog.class);
+
+        if (StringUtils.isNotBlank(orderBy)) {
+            if (asc) {
+                detachedCriteria.addOrder(Order.asc(orderBy));
+            } else {
+                detachedCriteria.addOrder(Order.desc(orderBy));
+            }
+        }
+
+        offset = IntegerUtils.boundsChecking(offset);
+        perPage = IntegerUtils.boundsChecking(perPage);
+
+        final List<RestServiceLog> byCriteria = this.getHibernateTemplate().findByCriteria(detachedCriteria, offset, perPage);
+
+        if (byCriteria.size() == 0) {
+            return null;
+        }
+
+        return byCriteria;
+    }
+
+    @Override
+    public Number countSearchResultsWithFilters(String search){
+        DetachedCriteria detachedCriteria = DetachedCriteria
+                .forClass(RestServiceLog.class)
+                .setProjection(Projections.rowCount());
+
+        Number totalResults;
+
+        try {
+            totalResults = (Number) this.getHibernateTemplate().findByCriteria(detachedCriteria).get(0);
+        } catch (Exception e) {
+            totalResults = null;
+            System.out.println(e);
+        }
+
+        return totalResults;
     }
 
 }
---
+++ src/main/java/alexberemart/socialNBA/rest/RestServiceLogService.java
@@ -0,0 +1,41 @@
+package alexberemart.socialNBA.rest;
+
+import Alexberemart.core.rest.AbstractRestService;
+import alexberemart.socialNBA.model.vo.RestServiceLog;
+import alexberemart.socialNBA.services.PlayerEntryServices;
+import alexberemart.socialNBA.services.RestServiceLogServices;
+import alexberemart.socialNBA.util.BootstrapTableUtils;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Path("restServiceLog")
+public class RestServiceLogService extends AbstractRestService {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBasketReferenceInfo(
+            @QueryParam("sort") String orderBy,
+            @QueryParam("order") String order,
+            @QueryParam("offset") Integer offset,
+            @QueryParam("limit") Integer perPage,
+            @QueryParam("search") String search
+    ) throws IOException {
+
+        Boolean asc = BootstrapTableUtils.getBooleanOrder(order);
+
+        List<RestServiceLog> restServiceLogList = RestServiceLogServices.getInstance().findWithFiltersPaginated(orderBy, offset, perPage, asc, search);
+        Map<String, Object> result = new HashMap<>();
+        result.put("rows", restServiceLogList);
+        result.put("total", RestServiceLogServices.getInstance().countSearchResults(search));
+        return ok(result);
+    }
+}
